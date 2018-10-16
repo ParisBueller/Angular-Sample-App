104b6117c7bef05a8a7a4e63de70133b05f9e348
Paris Ward <wardparis.pw@gmail.com>
2018-10-15 23:49:20 -0700

    added dropdown select

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { FlashMessagesService } from 'angular2-flash-messages';
 
 import { User } from '../../../models/User';
+import { FormGroup, FormControl } from '@angular/forms';
 
 
 @Component({
@@ -17,17 +18,35 @@ export class FormComponent implements OnInit {
     password: ''
   }
 
+  show: boolean;
 
+  subscriptions = [ 'Basic', 'Advanced', 'Pro'];
+  default: string = "Advanced";
+
+  subscriptionForm: FormGroup;
 
   @ViewChild('userForm') form: any;
 
   constructor(private flashMessage: FlashMessagesService,
               private router: Router
-    ) { }
+    ) { 
+      // set show/toggle password 
+      this.show = false;
+      //set default value of dropdown subscription select
+      this.subscriptionForm = new FormGroup({
+        subscription: new FormControl(null)
+      });
+      this.subscriptionForm.controls['subscription'].setValue(this.default, {onlySelf: true});
+    }
 
   ngOnInit() {
   }
 
+  //Toggle show/hide password
+  toggleShow() {
+    this.show = !this.show;
+  }
+  //submit form
   onSubmit({valid}: {valid: boolean}) {
     if (!valid) {
       this.flashMessage.show('Please fill out the form correctly', {
